fe206a9c97f6ca2867fd463841eb1b09fc4bd1c6
Maksymus <39418562+maksymmm@users.noreply.github.com>
2026-09-05 07:59:07 -0700

    Export career run result

--- career_agent_ai/application/career/__init__.py
+++ career_agent_ai/application/career/__init__.py
@@ -1,11 +1,12 @@
 """Career orchestration and decision-making primitives."""
 
-from .career_orchestrator import CareerOrchestrator
+from .career_orchestrator import CareerOrchestrator, CareerRunResult
 from .career_plan import CareerPlan, CareerPlanStep
 from .career_step_result import CareerStepResult
 
 __all__ = [
     "CareerOrchestrator",
+    "CareerRunResult",
     "CareerPlan",
     "CareerPlanStep",
     "CareerStepResult",
